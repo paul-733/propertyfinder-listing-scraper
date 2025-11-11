b567390717de4777b8adc7abb0c93e0b65bd6e48
paul-733 <paulrose8050@gutkai.com>
2025-11-11 15:01:40 -0600

    feat: add runner.py

---
+++ runner.py
@@ -0,0 +1,17 @@
+## Example Output
+
+[
+{
+"id": "13695167",
+"property_type": "Villa",
+"price": {
+"value": 7600000,
+"currency": "AED",
+"period": "sell"
+},
+"title": "Iconic Location | High End 4BR Villa | Park View",
+"location": {
+"full_name": "Saadiyat Lagoons, Saadiyat Island, Abu Dhabi"
+}
+}
+]
